78595deb289989440ec997d2ce4ea6255d0bf531
Kivanc <kivancguncavdi35@gmail.com>
2022-06-21 01:51:42 +0300

    SDL_WaitEvent is used instead of SDL_PollEvent for performance optimization.

--- game.cpp
+++ game.cpp
@@ -35,7 +35,7 @@ int main() {
 void handleEvents() {
 	//the only event we'll check is the  SDL_QUIT event.
 	SDL_Event event;
-	SDL_PollEvent(&event);
+	SDL_WaitEvent(&event);
 
 	switch (event.type) {
 	case SDL_QUIT:
